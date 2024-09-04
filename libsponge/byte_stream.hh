318a3825911767c39ec7bfc51eaba668c7705b5c
xxh <2943110025@qq.com>
2024-09-04 17:10:46 +0800

    比特流读写

--- libsponge/byte_stream.hh
+++ libsponge/byte_stream.hh
@@ -2,7 +2,8 @@
 #define SPONGE_LIBSPONGE_BYTE_STREAM_HH
 
 #include <string>
-
+#include <buffer.hh>
+#include <iostream>
 //! \brief An in-order byte stream.
 
 //! Bytes are written on the "input" side and read from the "output"
@@ -16,7 +17,14 @@ class ByteStream {
     // all, but if any of your tests are taking longer than a second,
     // that's a sign that you probably want to keep exploring
     // different approaches.
+    // 用于存储填充进去的数据
+    BufferList dataStream{};
 
+    size_t cap;
+    size_t writeByte=0;
+    size_t readByte=0;
+
+    bool _end_input{}; //! Signal that the byte stream has reached its ending
     bool _error{};  //!< Flag indicating that the stream suffered an error.
 
   public:
