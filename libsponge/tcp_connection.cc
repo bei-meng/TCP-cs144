82e8b55f372a264fef4fdbd27247fb13453e4a30
xxh <2943110025@qq.com>
2024-12-29 21:34:38 +0800

    tcp连接的bug修复

--- libsponge/tcp_connection.cc
+++ libsponge/tcp_connection.cc
@@ -36,14 +36,12 @@ void TCPConnection::reset_connection() {
 }
 
 void TCPConnection::segment_received(const TCPSegment &seg) {
-    cerr<<"收到信号了吗222\n";
     // 非启动时不接收
     if(!active_){
         return;
     }
-    // cout<<"是否有FIN信号2222"<<seg.header().fin<<seg.length_in_sequence_space()<<endl;
+
     // 重置连接时间
-    if(seg.length_in_sequence_space()>0)
     time_since_last_segment_received_  = 0;
 
     TCPHeader header=seg.header();
@@ -52,77 +50,60 @@ void TCPConnection::segment_received(const TCPSegment &seg) {
         _receiver.stream_out().set_error();
         _sender.stream_in().set_error();
         active_ = false;
-        // 断开连接
-    }else if(state() == TCPState::State::CLOSED){
-        if(header.syn){
-            _receiver.segment_received(seg);
-            _sender.fill_window();
-            send_data();
-        }
-    }else if(state() == TCPState::State::LISTEN){
-        cerr<<"收到信号了吗\n";
-        // cout<<"LISTEN or CLOSED"<<endl;
+    }else if(state() == TCPState::State::LISTEN || state() == TCPState::State::CLOSED){
         // 等待接收SYN信号
         if(header.syn){
             _receiver.segment_received(seg);
-            _sender.send_empty_segment();
-            send_data();
+            connect();
         }
     }else if(state() == TCPState::State::SYN_SENT){
-        
-        // 收到了SYN信号，但是没有ACK，说明是同时建立连接
-        // 收到SYN信号，有ACK信号，说明是对面对SYN信号的确认
-        // 等待接收对SYN的确认
-        // cout<<"SYN_SENT"<<header.syn<<"   "<<header.ack<<endl;
+        // 等待接收对SYN的确认，以及SYN信号
+        // 如果只有syn信号，说明是同时开启连接，如果syn+ack是说明是确认信号
         if(header.syn){
             _receiver.segment_received(seg);
             if(header.ack){_sender.ack_received(header.ackno,header.win);}
-            // ！！！！！！这里要回复一个空段
             _sender.send_empty_segment();
             send_data();
         }
     }else if(state() == TCPState::State::SYN_RCVD){
-        // cout<<"SYN_RCVD"<<endl;
         // 接收到SYN信号，并且发送了SYN信号
         _receiver.segment_received(seg);
-        if(header.ack)_sender.ack_received(header.ackno,header.win);
+        if(header.ack){
+            _sender.ack_received(header.ackno,header.win);
+            _sender.fill_window();
+            send_data();
+        }
     }else if(state() == TCPState::State::ESTABLISHED){
-        // cout<<"ESTABLISHED"<<endl;
-        // 正常等待数据的接收和发送
-        _receiver.segment_received(seg);
         if(header.ack)_sender.ack_received(header.ackno,header.win);
+         _receiver.segment_received(seg);
         // 占序列号的要回复
-        // cout<<"为什么22"<<endl;
-        if(seg.length_in_sequence_space()>0){
-            // cout<<"为什么"<<endl;
+        _sender.fill_window();
+        if(seg.length_in_sequence_space()>0 && _sender.segments_out().empty()){
             _sender.send_empty_segment();
         }
-        _sender.fill_window();
+        
         send_data();
     }else if(state() == TCPState::State::FIN_WAIT_1){
         // 已经发出去FIN信号了，等待接收对FIN的确认
-        // cout<<"FIN_WAIT_1"<<endl;
-        if(seg.header().fin){
-            // 收到Fin，则发送新ACK，进入Closing/Time-Wait
+        if(header.fin){
+            // 收到fin信号，就要进入wait阶段，需要回复一个段确认FIN信号
             _receiver.segment_received(seg);
             if(header.ack)_sender.ack_received(header.ackno,header.win);
             _sender.send_empty_segment();
             send_data();
-        }else if(seg.header().ack){
-            // 收到ACK，进入Fin-Wait-2
-            _sender.ack_received(seg.header().ackno, seg.header().win);
-           _receiver.segment_received(seg);
-           send_data();
+        }else if(header.ack){
+            // 收到确认就不用回！！！！
+            _receiver.segment_received(seg);
+            if(header.ack)_sender.ack_received(header.ackno,header.win);
+            send_data();
         }
     }else if(state() == TCPState::State::FIN_WAIT_2){
         // 发出的FIN已经被确认了，等待接收到FIN信号
-        // cout<<"FIN_WAIT_2"<<endl;
         _receiver.segment_received(seg);
-        if(header.ack)_sender.ack_received(header.ackno,header.win);
+        if(header.ack) _sender.ack_received(header.ackno,header.win);
         _sender.send_empty_segment();
         send_data();
     }else if(state() == TCPState::State::TIME_WAIT){
-        // cout<<"FIN_WAIT"<<endl;
        if (seg.header().fin) {
            // 收到FIN，保持Time-Wait状态
            _sender.ack_received(seg.header().ackno, seg.header().win);
@@ -131,10 +112,14 @@ void TCPConnection::segment_received(const TCPSegment &seg) {
            send_data();
        }
     }else if(state() == TCPState::State::CLOSE_WAIT){
-        // 已经接收到FIN伦理，但是没有发送FIN信号，等待发送FIN信号
+        // 已经接收到FIN确认了，但是没有发送FIN信号，等待发送FIN信号
         _sender.ack_received(seg.header().ackno, seg.header().win);
         _receiver.segment_received(seg);
-        _sender.send_empty_segment();
+        // 这里可能存在需要继续发的数据，没有数据就需要回一个ACK!!!!!!!!!
+        _sender.fill_window();
+        if(seg.length_in_sequence_space()>0 && _sender.segments_out().empty()){
+            _sender.send_empty_segment();
+        }
         send_data();
     }else if(state() == TCPState::State::LAST_ACK){
         // 接收到FIN了，并且发送了FIN，等待收到ACK
@@ -149,12 +134,6 @@ void TCPConnection::segment_received(const TCPSegment &seg) {
         _sender.fill_window();
         send_data();
     }
-    // // if(_receiver.ackno().has_value() && (seg.length_in_sequence_space()==0) && (header.seqno == (_receiver.ackno().value()-1))){
-    // //     _sender.send_empty_segment();
-    // // }
-    // _sender.fill_window();
-    // cout<<"下空"<<_sender.segments_out().empty()<<endl;
-    // send_data();
 }
 
 bool TCPConnection::active() const {
@@ -174,14 +153,12 @@ void TCPConnection::tick(const size_t ms_since_last_tick) {
     if (!active()) {
        return;
     }
-    // cout<<"过去多久了"<<ms_since_last_tick<<" "<<time_since_last_segment_received_<<endl;
     time_since_last_segment_received_ += ms_since_last_tick;
     _sender.tick(ms_since_last_tick);
-    // ！！！！！！！！！！注意，这里是大于
-   if (_sender.consecutive_retransmissions() > TCPConfig::MAX_RETX_ATTEMPTS) {
-       reset_connection();
-       return;
-   }
+    if (_sender.consecutive_retransmissions() > TCPConfig::MAX_RETX_ATTEMPTS) {
+        reset_connection();
+        return;
+    }
     send_data();
 }
 
@@ -199,7 +176,6 @@ void TCPConnection::connect() {
 }
 
 void TCPConnection::send_data(){
-    cerr<<"发送信号了没\n";
     // sender管SYN，FIN信号，还有seqno,receiver管win和ack标记，ackno
     while(!_sender.segments_out().empty()){
        TCPSegment seg = _sender.segments_out().front();
@@ -211,21 +187,22 @@ void TCPConnection::send_data(){
            seg.header().win = _receiver.window_size();
        }
        _segments_out.push(seg);
-
     }
-    // 发送完毕要结束链接
     if(_receiver.stream_out().input_ended()){
-        // 已经全部接收了
+        // 已经接收到FIN信号了
+        
         if(!_sender.stream_in().eof()){
-            // 但是没有全部发送过去
+            // 但是输出流没有发完
             _linger_after_streams_finish = false;
-        }else if(_sender.bytes_in_flight() == 0){
-            // ！！！！！！！！！！注意，这里是等于
-            if (!_linger_after_streams_finish || (time_since_last_segment_received() >= 10 * _cfg.rt_timeout)) {
-               active_ = false;
-           }
+            // 说明可以不用逗留，是对面先发起的FIN信号
+        }else if(_sender.bytes_in_flight()==0){
+            // 输出流已经发完了，如果是不用逗留的情况，直接结束，如果是用逗留的情况，超时结束
+            if((!_linger_after_streams_finish)||(time_since_last_segment_received() >= (10 * _cfg.rt_timeout))){
+                active_ = false;
+            }
         }
     }
+
 }
 
 TCPConnection::~TCPConnection() {
